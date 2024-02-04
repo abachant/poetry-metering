72b660101de8d8b5579447738347b7ac42be51cf
abachant <aubreybachant@gmail.com>
2024-02-03 23:52:05 -0500

    Add path handling for js and css

--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
-import {serveFile } from "https://deno.land/std@0.207.0/http/file_server.ts";
+import {
+  serveDir,
+  serveFile,
+} from "https://deno.land/std@0.207.0/http/file_server.ts";
 
 Deno.serve((req: Request) => {
-
   const pathname = new URL(req.url).pathname;
-  if (pathname === "/") {
-
-    return serveFile(req, "./index.html");
+  console.log(pathname);
+  if (pathname === "/" || pathname === "index.html") {
+    return serveFile(req, "./public/index.html");
+  }
+  if (pathname === "/index.js") {
+    return serveFile(req, "./public/index.js");
+  }
+  if (pathname === "/style.css") {
+    return serveFile(req, "./public/style.css");
   }
 
   return new Response("404: Not Found", {
