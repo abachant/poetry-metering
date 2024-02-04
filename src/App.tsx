879e195b2389ddae7f19bfe3ec97a90617db95a1
abachant <aubreybachant@gmail.com>
2024-02-04 18:29:07 -0500

    Add state for textinput value

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import { useState } from "react";
 
 function App() {
   const [syllableCount, setSyllableCount] = useState(0);
+  const [textInput, setTextInput] = useState("");
+
   return (
     <div className="App">
       <div className="text-input-container">
         <div className="syllable-count">{syllableCount}</div>
-        <textarea name="text-input"></textarea>
+        <textarea
+          name="text-input"
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+        ></textarea>
       </div>
     </div>
   );
